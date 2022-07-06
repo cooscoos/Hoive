9ae30071da4c044b12df347fe926fa9ff570c00f
cooscoos <m.perry@strath.ac.uk>
2022-07-06 10:24:38 +0100

    up

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use Hoive::{Animal, Piece, Player, Team};
+
 fn main() {
-    println!("Hello, world!");
+    // Start with a 1 player game where we just place pieces correctly
+    // Add in a second player to place pieces correctly
+    // Then do movement
+
+    // initialise a player
+    let P1 = Player::default(Team::Black);
+
+    // show the player's hand
+    println!("{:?}", P1.show_hand());
+
+    // let them place a piece at 0,0,0, use a command to show hand
+    
+
 }
