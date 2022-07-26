8e66d44e934b94ddcfb7b9dc5ffd26f0b47fb6e6
cooscoos <m.perry@strath.ac.uk>
2022-07-26 09:06:38 +0100

    comments

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,14 @@ use hoive::game::board::{Board, MoveStatus};
 use hoive::pmoore;
 
 fn main() {
-    // Initialise a game board using a cube co-ordinate system for hexes
-    // The game board comes with 4 spiders, s1,s2,...,s4 for each team
+    // Initialise game board in cube co-ordinates
     let coord = hoive::maths::coord::Cube;
     let mut board = Board::default(coord);
 
-    // Say hello and tell the player who is going first
+    // Say hello, tell players who goes first
     let first = pmoore::intro();
 
-    // Game loop
+    // Loop game until someone wins
     loop {
         if let MoveStatus::Win(_) = pmoore::take_turn(&mut board, first) {
             break;
