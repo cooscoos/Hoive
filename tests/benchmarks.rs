cda05e28b3c2c43561deec323f1acbdc32ea207c
cooscoos <m.perry@strath.ac.uk>
2022-08-03 12:30:39 +0100

    up

--- tests/benchmarks.rs
+++ tests/benchmarks.rs
@@ -1,8 +1,9 @@
 // To run these tests
-// cargo test benchmark --features benchmarking
+// cargo test benchmark --features benchmarking -- --nocapture
 
 #[test]
 #[cfg_attr(not(feature= "benchmarking"), ignore)]
 fn benchmark_1() {
+    println!("BOOYAH");
     panic!("BAD TOUCH");
 }
