4e5662c6389675d5b1e745ff9e6ecdb2170a5366
cooscoos <m.perry@strath.ac.uk>
2022-08-23 08:53:13 +0100

    up

---
+++ src/api.rs
@@ -0,0 +1,25 @@
+/// API is the middleman between the game's logic and the front-end. It converts string commands from the front
+/// end into commands the board understands, and converts responses from the board into human-readable strings.
+/// 
+
+use crate::game::{board::Board, movestatus::MoveStatus};
+use crate::maths::coord::Cube;
+use crate::maths::coord::Coord;
+
+/// Start a new game, create a db respond with how it went
+fn new_game() {
+    // Initialise game board in cube co-ordinates
+    let coord = Cube::default();
+    let mut board = Board::new(coord);
+
+}
+
+// We need a way of storing a board as a string in an sqlitedb
+// need a table called gamestate which has:
+// session id, a board (string representing board), user1, user2, current-player, ended (bool)
+
+// Then have the option to find an existing session without a user2 and join it as a player
+
+
+
+
--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ pub mod draw;
 pub mod game;
 pub mod maths;
 pub mod pmoore;
+pub mod api;
+pub mod gameshow_host;
